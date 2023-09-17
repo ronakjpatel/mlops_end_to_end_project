94ab225311d81280920c20956aabf936f77760b2
ronakjpatel <40088060+ronakjpatel@users.noreply.github.com>
2023-09-17 13:20:20 -0400

    Added the required functionality for the data ingestion pipeline

---
+++ src/end_to_end_mlops/constants/__init__.py
@@ -0,0 +1,13 @@
+'''
+-------------- Information about this file ------------------------
+
+This file contains the CONSTANTS for example file paths that we might use. 
+---------------------------------------------------------------------
+'''
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
+
