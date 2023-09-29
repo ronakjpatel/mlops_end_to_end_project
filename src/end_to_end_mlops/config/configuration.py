8486779ff2b61aca957d409316e0061d3c68bac2
ronakjpatel <40088060+ronakjpatel@users.noreply.github.com>
2023-09-29 14:38:28 -0400

    added data validation pipeline logic

--- src/end_to_end_mlops/config/configuration.py
+++ src/end_to_end_mlops/config/configuration.py
@@ -2,7 +2,7 @@
 from end_to_end_mlops.constants import *
 #Importing the predefined common methods from utils
 from end_to_end_mlops.utils.common import read_yaml,create_directories
-from end_to_end_mlops.entity.config_entity import DataIngestionConfig
+from end_to_end_mlops.entity.config_entity import DataIngestionConfig,DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -32,3 +32,19 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
